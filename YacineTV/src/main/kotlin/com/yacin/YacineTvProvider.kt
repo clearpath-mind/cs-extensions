f9eda2a23ace949041d543187bd55bcf5c5db3f3
clearpath-mind <clearpath-mind@users.noreply.github.com>
2026-09-10 15:29:50 +0100

    YacineTV: add missing loadExtractor import

--- YacineTV/src/main/kotlin/com/yacin/YacineTvProvider.kt
+++ YacineTV/src/main/kotlin/com/yacin/YacineTvProvider.kt
@@ -20,6 +20,7 @@ import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.ExtractorLinkType
 import com.lagradost.cloudstream3.utils.Qualities
 import com.lagradost.cloudstream3.utils.getQualityFromName
+import com.lagradost.cloudstream3.utils.loadExtractor
 import com.lagradost.cloudstream3.utils.newExtractorLink
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
